77dbc8245cbf530665339b9609943d7f4e968b75
Andrew Moldovan <anmoldov@microsoft.com>
2015-12-08 19:54:36 -0800

    can now add and remove data from the line chart

--- src/app/components/d3/d3MultiLineChartHelper.service.ts
+++ src/app/components/d3/d3MultiLineChartHelper.service.ts
@@ -2,7 +2,8 @@ module moddynBlog {
   'use strict';
 
   export class d3MultiLineChartHelperService{
-    
+    public deletedData = [];
+
     /** @ngInject */
     constructor(private d3HelperService: any) { }
 
@@ -17,15 +18,16 @@ module moddynBlog {
       var line = this.createLine(d3, x, y, "date", "litres");
       this.createColorDomain(d3, color, data);
       this.parseDates(data, parseDate, "Ref_Date");
-      var milk = this.createDataArray(color, data, "Ref_Date");
+      var dataArr = this.createDataArray(color, data, "Ref_Date");
 
       this.createXDomain(d3, x, data, "Ref_Date");
-      this.createYDomain(d3, y, milk, "litres");
+      this.createYDomain(d3, y, dataArr, "litres");
 
       this.drawXAxis(svg, xAxis, height);
       this.drawYAxis(svg, yAxis, 'Litres drunk per capita (L)');
-      this.drawLine(svg, milk, color, x, y, line, "date", "litres");
+      this.drawLine(svg, dataArr, color, x, y, line, "date", "litres");
       this.d3HelperService.createTitle(svg, width, margin, title);
+      this.createButtons(d3, svg, width, margin, dataArr, color, x, y, line, 'date', 'litres');
     }
 
     public createLine(d3, x, y, xProp, yProp) {
@@ -113,26 +115,80 @@ module moddynBlog {
     }
 
     public drawLine(svg, data, color, x, y, line, dateProp, valueProp) {
-      var city = svg.selectAll(".city")
-        .data(data)
-        .enter().append("g")
-        .attr("class", "city");
+      // create a holder for each line in the multi-line chart
+      var eachLine = svg.selectAll(".eachLine")
+        .data(data, function(d) {
+          return d.name;
+        });
 
-      city.append("path")
+      var eachLineEnter = eachLine.enter()
+        .append("g")
+        .attr("class", "eachLine");
+
+      // create a line for each one
+      eachLineEnter.append("path")
         .attr("class", "line")
         .attr("d", function(d) {
+          // the line function takes in an array of values and draws the whole thing in one go
           return line(d.values);
         })
         .style("stroke", function(d) {
           return color(d.name);
         });
 
-      city.append("text")
-        .datum(function(d) { return { name: d.name, value: d.values[d.values.length - 1] }; })
+      eachLineEnter.append("text")
+        .datum(function(d) { 
+          return { name: d.name, value: d.values[d.values.length - 1] }; 
+        })
         .attr("transform", function(d) { return "translate(" + x(d.value[dateProp]) + "," + y(d.value[valueProp]) + ")"; })
         .attr("x", 3)
         .attr("dy", ".35em")
         .text(function(d) { return d.name; });
+
+      eachLine.exit().remove();
+    }
+
+    public createButtons(d3, svg, width, margin, data, color, x, y, line, dateProp, valueProp) {
+      var optionsStart = 20;
+      var that = this;
+      var options = svg.selectAll(".option")
+        .data(data, function(d) {
+          return d.name;
+        })
+        .enter()
+        .append("g");
+
+      options.append('text')
+        .attr('x', function(d, i) { 
+          return ((d.name.length * 10) + parseInt(i)*70)
+        })
+        .attr('y', 0)
+        .attr('class', 'option-text')
+        .attr('fill', 'blue')
+        .text(function(d) { return d.name })
+        .on("click", function(d) {
+          if (that.deletedData[d.name]) {
+            d3.select(this).attr("fill", 'blue');
+            that.addDataLine(that.deletedData[d.name], data);
+            that.drawLine(svg, data, color, x, y, line, dateProp, valueProp);
+          } else {
+            d3.select(this).attr("fill", 'black');
+            that.removeDataLine(data, d, that);
+            that.drawLine(svg, data, color, x, y, line, dateProp, valueProp);
+          }
+        });
+    }
+
+    public addDataLine(dataToAdd, data) {
+      data.push(dataToAdd);
+    }
+
+    public removeDataLine(data, d, that) {
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].name === d.name) {
+          that.deletedData[d.name] = (data.splice(i, 1)[0]);
+        }
+      }
     }
   }
 }
